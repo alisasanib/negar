cd3cb0a96ed77ac30822061596b7d3534f2c9601
Ali Sasani Babak <ababak@nuclera.com>
2023-09-17 16:20:34 +0100

    fix width

--- src/components/CustomizedModalStoryBoard.jsx
+++ src/components/CustomizedModalStoryBoard.jsx
@@ -12,7 +12,7 @@ const style = {
   left: "50%",
   transform:
     "translate(-50%, -50%)",
-  width: 1400,
+  width: "90vw",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
