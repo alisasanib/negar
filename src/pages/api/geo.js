ab8d7207ebadef59b74ebf6bc24295784e8b9e83
Ali Sasani Babak <ababak@nuclera.com>
2023-12-02 19:14:38 +0000

    Add ip address

--- src/pages/api/geo.js
+++ src/pages/api/geo.js
@@ -1,5 +1,8 @@
 import requestIp from "request-ip";
-import { geolocation } from "@vercel/edge";
+import {
+  geolocation,
+  ipAddress,
+} from "@vercel/edge";
 
 export const config = {
   runtime: "edge",
@@ -9,16 +12,19 @@ export default function handler(
   req,
   res
 ) {
-  const { city } =
+  const { city, country } =
     geolocation(req);
 
+  const ip = ipAddress(req);
   const detectedIp =
     requestIp.getClientIp(
       req
     );
 
   return new Response(
-    `<h1>Your location is ${city}</h1>`,
+    `<h1>Your location is ${city}</h1>
+    <h1>Your country is ${country}</h1>
+    <h1>Your ip is ${ip}</h1>`,
     {
       headers: {
         "content-type":
